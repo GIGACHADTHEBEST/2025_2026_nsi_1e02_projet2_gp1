ff6d83705e493fab4e5a963e32e4d67edc78ab26
raphaelbtgm <raphael.btgm@gmail.com>
2026-01-19 12:21:18 +0100

    Update raph.py

--- raph.py
+++ raph.py
@@ -4,7 +4,7 @@ from tkinter import ttk, messagebox
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib.pyplot as plt
 
-print ("Bonjour, voulez-vous jouer à un jeu d'argent ?)")
+
 
 
 def load_csv(filepath):
