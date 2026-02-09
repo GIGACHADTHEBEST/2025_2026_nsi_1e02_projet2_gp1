a283721761d38c8f1a5832e60a07b9b5a9213075
Louis El Tantawi <louiseltantawi@gmail.com>
2026-02-09 12:49:57 +0100

    hhj

--- archi_mvc_projet_jeu_argent/main.py
+++ archi_mvc_projet_jeu_argent/main.py
@@ -1,4 +1,9 @@
 import tkinter as tk
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(__file__))  # ajoute le dossier archi_mvc_projet_jeu_argent au PATH
+
 from archi_mvc_projet_jeu_argent.model.catalogue import Catalogue
 from archi_mvc_projet_jeu_argent.controller.accueil_controller import AccueilController
 
