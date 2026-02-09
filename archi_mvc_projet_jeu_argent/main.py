69d235534191d9147596667d058801082b419f1b
jadarcenko <jermolaj1@gmail.com>
2026-02-09 13:10:12 +0100

    pb technique avec classe

--- archi_mvc_projet_jeu_argent/main.py
+++ archi_mvc_projet_jeu_argent/main.py
@@ -1,11 +1,8 @@
 import tkinter as tk
-import sys
 import os
 
-sys.path.insert(0, os.path.dirname(__file__))  # ajoute le dossier archi_mvc_projet_jeu_argent au PATH
-
-from archi_mvc_projet_jeu_argent.model.catalogue import Catalogue
-from archi_mvc_projet_jeu_argent.controller.accueil_controller import AccueilController
+from controller.accueil_controller import AccueilController
+from model.catalogue import Catalogue
 
 
 
