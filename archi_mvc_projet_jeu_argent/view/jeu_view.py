ff9decfe40b93c11b8794ce8e186c426d8ae80ab
Louis El Tantawi <louiseltantawi@gmail.com>
2026-02-12 21:21:17 +0100

    architecture mvc

--- archi_mvc_projet_jeu_argent/view/jeu_view.py
+++ archi_mvc_projet_jeu_argent/view/jeu_view.py
@@ -3,32 +3,29 @@ from tkinter import ttk
 
 
 class JeuView:
-    def __init__(self, root):
-        
-        self.root = root
-        self.root.title("Tester un jeu")
-        self.root.geometry("500x350")
-        self.root.resizable(False, False)
-        self.root.configure(bg="#ffce84")
+    def __init__(self, parent):
+        self.window = tk.Toplevel(parent)
+        self.window.title("Tester un jeu")
+        self.window.geometry("500x350")
 
         ttk.Label(
-            self.root,
+            self.window,
             text="Tester un jeu",
             font=("Helvetica", 16, "bold")
         ).pack(pady=20)
 
         # Choix du jeu
-        ttk.Label(self.root, text="Choisissez un jeu :").pack()
-        self.combo_jeu = ttk.Combobox(self.root, state="readonly", width=30)
+        ttk.Label(self.window, text="Choisissez un jeu :").pack()
+        self.combo_jeu = ttk.Combobox(self.window, state="readonly", width=30)
         self.combo_jeu.pack(pady=10)
 
         # Bouton jouer
-        self.btn_jouer = ttk.Button(self.root, text="🎟 Gratter un ticket")
+        self.btn_jouer = ttk.Button(self.window, text="🎟 Gratter un ticket")
         self.btn_jouer.pack(pady=15)
 
         # Résultat
         self.label_resultat = ttk.Label(
-            self.root,
+            self.window,
             text="",
             font=("Helvetica", 12)
         )
@@ -40,3 +37,4 @@ if __name__ == "__main__":
     root.withdraw()  # cache la fenêtre principale (optionnel)
     app = JeuView(root)
     root.mainloop()
+
